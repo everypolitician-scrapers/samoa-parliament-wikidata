954330231e6af6ec6c38a8e34f9f66053a79204c
Tony Bowden <tony@tmtm.com>
2015-10-24 23:20:46 +0100

    rebuild when done

--- scraper.rb
+++ scraper.rb
@@ -24,6 +24,10 @@ WikiData.ids_from_pages('en', members.map { |c| c[:wikiname] }).each_with_index 
     warn "No data for #{p}"
     next
   end
+  data[:orig] = p.first
   ScraperWiki.save_sqlite([:id], data)
 end
+
+warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+
 
